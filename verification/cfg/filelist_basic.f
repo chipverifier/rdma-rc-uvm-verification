55d141c079bf3d5e729fb9e8d1c871d47c6b5256
chipverifier <2718214517@qq.com>
2025-12-14 16:22:45 +0800

    add filelist

---
+++ verification/cfg/filelist_basic.f
@@ -0,0 +1,20 @@
+// RTL
+../../rtl/rx/rdma_rx.v
+../../rtl/qp/qp_context.v
+../../rtl/sched/rdma_sched.v
+../../rtl/tx/rdma_tx.v
+../../rtl/top/rdma_top.v
+
+//common
+../common/common_pkg.sv
+
+//env
+../../verification/env/rdma_monitor.sv
+../../verification/env/rdma_driver.sv
+../../verification/env/rdma_env.sv
+//tc
+../../verification/tc/rdma_test.sv
+//tb
+../../verification/tb/tb_rdma_top.sv
+../../verification/tb/rdma_if.sv
+
